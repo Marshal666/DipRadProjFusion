a2674cbd5f978986d86841e0d0be2b12691a015d
Adam Kolar <akolar@adscanner.tv>
2023-11-26 13:52:24 +0100

    w++

--- Assets/Scripts/PhysicsDoubleRaycast/DoubleRaycasting.cs
+++ Assets/Scripts/PhysicsDoubleRaycast/DoubleRaycasting.cs
@@ -8,55 +8,26 @@ using UnityEngine;
 public static class DoubleRaycasting
 {
 
-    public static RaycastHit[] DoubleRaycastAll(Vector3 origin, Vector3 direction, float distance = Mathf.Infinity, int layerMask = int.MaxValue,
+    public static RaycastHit[] DoubleRaycastAll(Vector3 origin, Vector3 direction, float distance = Mathf.Infinity, 
+        int layerMask = int.MaxValue,
         QueryTriggerInteraction query = QueryTriggerInteraction.UseGlobal)
     {
         Vector3 start2 = origin + direction.normalized * distance;
         RaycastHit[] forwardHits = Physics.RaycastAll(origin, direction, distance, layerMask, query);
         RaycastHit[] backwardHits = Physics.RaycastAll(start2, -direction, distance, layerMask, query);
-        
-        Array.Sort(forwardHits, (RaycastHit a, RaycastHit b) =>
-            Mathf.CeilToInt(a.distance - b.distance));
-        
-        //Debug.Log($"fwdh: {forwardHits.ToStr(", ")}");
-        
-        Array.Sort(backwardHits, (RaycastHit a, RaycastHit b) =>
-            Mathf.CeilToInt((distance - a.distance) - (distance - b.distance)));
-        
-        //Debug.Log($"bwdh: {backwardHits.ToStr(", ")}");
+
+        for (int i = 0; i < backwardHits.Length; i++)
+        {
+            backwardHits[i].distance = distance - backwardHits[i].distance;
+        }
         
         RaycastHit[] ret = new RaycastHit[forwardHits.Length + backwardHits.Length];
 
-        int fi = 0;
-        int bi = 0;
-
-        for (int i = 0; i < ret.Length; i++)
-        {
-            if (fi < forwardHits.Length)
-            {
-                if (bi < backwardHits.Length)
-                {
-                    if (forwardHits[fi].distance < (distance - backwardHits[bi].distance))
-                    {
-                        ret[i] = forwardHits[fi++];
-                    }
-                    else
-                    {
-                        backwardHits[bi].distance = distance - backwardHits[bi].distance;
-                        ret[i] = backwardHits[bi++];
-                    }
-                }
-                else
-                {
-                    ret[i] = forwardHits[fi++];
-                }
-            }
-            else
-            {
-                backwardHits[bi].distance = distance - backwardHits[bi].distance;
-                ret[i] = backwardHits[bi++];
-            }
-        }
+        forwardHits.CopyTo(ret, 0);
+        backwardHits.CopyTo(ret, forwardHits.Length);
+        
+        Array.Sort(ret, (RaycastHit a, RaycastHit b) => 
+            Mathf.CeilToInt((a.distance - b.distance) * 100000f));
         
         //Debug.Log(ret.ToStr(", "));
         
